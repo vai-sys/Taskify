77425d59fdeac6c0e9cb6fd5255e3358a6012619
Vaishnavi Thorat <vaishnavit582@gmail.com>
2024-12-05 16:42:29 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: process.env.FRONTEND_URI,
   credentials: true
 }));
 
