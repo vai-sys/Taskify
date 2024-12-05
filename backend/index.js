093463173dc4004a7bfbe8087df5f98b603754b6
Vaishnavi Thorat <vaishnavit582@gmail.com>
2024-12-05 21:35:15 +0530

    Deploy

--- backend/index.js
+++ backend/index.js
@@ -15,10 +15,15 @@ const port = process.env.PORT || 5000;
 connectDB();
 app.use(express.json());
 app.use(cookieParser());
+const allowedOrigins = process.env.NODE_ENV === 'production'
+  ? ["https://taskify-frontend-pied.vercel.app"]
+  : ["http://localhost:5173"];
+
 app.use(cors({
-  origin:  "http://localhost:5173",
-  credentials: true
+  origin: allowedOrigins,
+  credentials: true,
 }));
+
 
 
 app.use("/api/auth", authRoutes);
